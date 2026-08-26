3dd427bf0912585a56ed71d17786e4a0f090fb5d
gamjya0o <wlsrhkd420@gmail.com>
2026-08-26 16:12:00 +0900

    Delete codon_table.py

--- codon_table.py
+++
@@ -1,7 +0,0 @@
-# Uploaded codon optimization table (20 amino acids -> single optimized codon)
-CODON_TABLE = {
-    'F': 'TTC', 'Y': 'TAC', 'C': 'TGC', 'W': 'TGG', 'L': 'CTG',
-    'P': 'CCC', 'H': 'CAC', 'Q': 'CAG', 'I': 'ATC', 'M': 'ATG',
-    'T': 'ACC', 'N': 'AAC', 'K': 'AAG', 'S': 'AGC', 'R': 'AGG',
-    'V': 'GTC', 'A': 'GCC', 'D': 'GAC', 'E': 'GAG', 'G': 'GGC',
-}
